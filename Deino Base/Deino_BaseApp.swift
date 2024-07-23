bf217755300456ed2a9c58344085721a49361d13
TheBot99 <102336081+TheBot99@users.noreply.github.com>
2024-07-23 11:04:59 -0500

    Initial Commit

---
+++ Deino Base/Deino_BaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deino_BaseApp.swift
+//  Deino Base
+//
+//  Created by Vinay Maryala on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Deino_BaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
